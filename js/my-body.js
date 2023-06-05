2e72486fc04f4971578be80b8539993723bc50bb
Angel Velasco <angelgg2020@outlook.com>
2023-06-05 09:10:15 -0500

    feat: :construction: Building add and minus buttons}

--- js/my-body.js
+++ js/my-body.js
@@ -4,17 +4,34 @@ import styles from 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootst
 export class Body extends HTMLElement {
     constructor() {
         super();
+        document.adoptedStyleSheets.push(styles);
     }
 
     async components() {
         return await (await fetch('components/my-body.html')).text();
     }
+    selection(e) {
+        let target = e.target;
+        if(target.nodeName == 'BUTTON') {
+            let allInputs = document.querySelectorAll(`#${$dataset.row} input`);
+
+            switch(target) {
+                case "-":
+                    allInputs.forEach(e => {
+                        
+                    })
+
+            }
+        }
+    }
 
     connectedCallback() {
-        document.adoptedStyleSheets.push(styles);
         this.components().then(html => {
-            this.innerHTML = html
+            this.innerHTML = html;
+            this.container = this.querySelector('#products');
+            this.container.addEventListener('click', this.selection);
         })
-    } 
+        
+    }
 }
 customElements.define('my-body', Body);
