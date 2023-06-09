8ded60779b8edd6b425e6a067056d2ccd812d7c7
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-06-09 17:07:36 -0500

    feat: :sparkles: Add the Basic Config and trait Singleton
    Add the headers, method and data to manage all http request. In the same way add the traid singleton to ensure that the next classes have a single instance and to have global access to that instance.

--- uploads/app.php
+++ uploads/app.php
@@ -1,4 +1,26 @@
 <?php
+    /* CONFIG */
+    header("Content-Type: application/json; charset=UTF-8");
+    $_HEADER = apache_request_headers();
+    $_METHOD = $_SERVER['REQUEST_METHOD'];
+    $_DATA = json_decode(file_get_contents('php://input', true));
+
+    /* Singleton */
+    trait Singleton {
+        private static $instance;
+        private function __construct($args) {}
+        public static function getInstance(...$args) {
+            return self::$instance ??= new static (...$args);
+        }
+
+        public function __set($name, $value) {
+            $this->$name = $value;
+        }
+        public function __get($name) {
+            return $this->$name;
+        }
+    }
+
     function autoLoad($class) {
         $allDirectories = [
             dirname(__DIR__).'/scripts/db/',
