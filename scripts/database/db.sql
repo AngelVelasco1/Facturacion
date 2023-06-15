2134000c76bb3d209f81069dc3980dc0cc9ecbc0
Angel Velasco <angelgg2020@outlook.com>
2023-06-15 09:12:32 -0500

    docs: :memo: Practice with DDL, DCL, DQL,  DML

--- scripts/database/db.sql
+++ scripts/database/db.sql
@@ -26,4 +26,19 @@ CREATE TABLE seller (
 seller_id INT,
    name VARCHAR(255) NOT NULL
 );
-ALTER TABLE bill ADD FOREIGN KEY (customer_id) REFERENCES customer(customer_id), ADD FOREIGN KEY (seller_id) REFERENCES seller(seller_id), ADD FOREIGN KEY (product_id) REFERENCES products(product_id);
+ALTER TABLE bill 
+ADD FOREIGN KEY (customer_id) REFERENCES customer(customer_id), 
+ADD FOREIGN KEY (seller_id) REFERENCES seller(seller_id), 
+ADD FOREIGN KEY (product_id) REFERENCES products(product_id);
+
+INSERT INTO customer (customer_id, fullName, email, phone, address) VALUES (1, 'Maria', 'maria@gmail.com', '+57 3784574112', 'Street 5');
+SELECT fullName AS "Full's_Names" FROM customer;
+
+
+
+USE db_hunter_facture;
+INSERT INTO tb_client (identificacion, full_name, email, address, phone) VALUES (2, 'Diego Norrea', 'angel@outlook.com', 'Street 4', '+57 2374111');
+SELECT * FROM tb_client WHERE identificacion = 2 or address = 'Campus';
+
+SELECT * FROM tb_client LIMIT 5 OFFSET 0
+;
